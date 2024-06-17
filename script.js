9f8640ef63f71349882dcf4fc7a1d79c943229a0
hodaya7 <hodayabig@gmail.com>
2024-06-17 13:23:30 +0300

    t

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// הסבר:
+// יצרתי 9 כפתורים בטבלה, בעת לחיצה על כל כפתור יוכנס לשם איקס או עיגןל בהתאמה ויעדכן את המטריצה
+// בכל פעם תתבצע בדיקה אם התור הוא של איקס או עיגול על ידי משתנה שאומר אם התור כעת זוגי או לא
+// ולאחר ההשמה תתבצע בדיקה במטריצה אם יש שורות או אלכסון עם אותו הסמל-ואז זה ניצחון
+
+//פנו אלי אם לא ברור משהו, תודה
 var board = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8]
-]
+];
+
 var turn = 1;
 
 function hasWon() {
@@ -70,24 +77,24 @@ function hasWinner(p1, p2, p3) {
 
 
 document.querySelector(".btn0").addEventListener("click", function () {
-    if (document.querySelector(".btn0").textContent == "") {
-        if (turn % 2 != 0) {
+    if (document.querySelector(".btn0").textContent == "") {//אם עוד אין חייל במיקום זה
+        if (turn % 2 != 0) {//אם התור אינו זוגי זה תור האיקס
             document.querySelector(".btn0").textContent = "x";
             board[0][0] = "x";
-            document.querySelector(".div-win").textContent = "o turn";
-            if (hasWon()) {
+            document.querySelector(".div-win").textContent = "o turn";//כתוב שכעת התור הוא של השחקן השני
+            if (hasWon()) {//בדיקה אם יש ניצחון במטריצה
                 document.querySelector(".div-win").textContent = hasWon() + " won";
                 var buttons = document.getElementsByTagName("button");
-                for (var i = 0; i < buttons.length-1; i++) {
+                for (var i = 0; i < buttons.length-1; i++) {//הפיכת הכםתורים לחסומים
                     buttons[i].disabled = true;
                 }
                
             }
-            else if(hasWon()==false&&turn==9){
+            else if(hasWon()==false&&turn==9){//אם הכל מלא ואין ניצחון
                 document.querySelector(".div-win").textContent = "tie";
             }
         }
-        else {
+        else {//אחרת-תור העיגול
             document.querySelector(".btn0").textContent = "o";
             board[0][0] = "o";
             document.querySelector(".div-win").textContent = "x turn";
@@ -102,12 +109,14 @@ document.querySelector(".btn0").addEventListener("click", function () {
                 document.querySelector(".div-win").textContent = "tie";
             }
         }
-        turn++;
+        turn++;//קידום התור
     }
-    else if (document.querySelector(".btn0").textContent != "")
-    alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
+    else if (document.querySelector(".btn0").textContent != ""&& turn!=10)//אם כבר יש חייל במקום הזה והמשחק עוד לא מלא-תתריע שהמיקום אינו פנוי
+    alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");//אם לא פנוי המיקום אך המשחק כן כולו מלא-לא יתריע על חוסר מיקום
 
 });
+
+//כפתור שני כנל:
 
 document.querySelector(".btn1").addEventListener("click", function () {
     if (document.querySelector(".btn1").textContent == "") {
@@ -143,7 +152,7 @@ document.querySelector(".btn1").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn1").textContent != "")
+    else if (document.querySelector(".btn1").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 
 });
@@ -182,7 +191,7 @@ document.querySelector(".btn2").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn2").textContent != "")
+    else if (document.querySelector(".btn2").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 });
 
@@ -220,7 +229,7 @@ document.querySelector(".btn3").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn3").textContent != "")
+    else if (document.querySelector(".btn3").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 });
 
@@ -258,7 +267,7 @@ document.querySelector(".btn4").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn4").textContent != "")
+    else if (document.querySelector(".btn4").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 });
 
@@ -293,7 +302,7 @@ document.querySelector(".btn5").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn5").textContent != "")
+    else if (document.querySelector(".btn5").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 });
 
@@ -331,7 +340,7 @@ document.querySelector(".btn6").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn6").textContent != "")
+    else if (document.querySelector(".btn6").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 });
 
@@ -369,7 +378,7 @@ document.querySelector(".btn7").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn7").textContent != "")
+    else if (document.querySelector(".btn7").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 });
 
@@ -407,8 +416,32 @@ document.querySelector(".btn8").addEventListener("click", function () {
         }
         turn++;
     }
-    else if (document.querySelector(".btn8").textContent != "")
+    else if (document.querySelector(".btn8").textContent != ""&& turn!=10)
     alert("אינך יכול להכניס כאן, הכנס במקום פנוי אחר");
 });
 
-// document.querySelector("#cell-0").textContent = 'x'
+//אתחול המשחק כשלוחצים שחק שוב
+document.querySelector(".restart").addEventListener("click", function(){
+    board = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+    turn = 1;
+    document.querySelector(".btn0").textContent = "";
+    document.querySelector(".btn1").textContent = "";
+    document.querySelector(".btn2").textContent = "";
+    document.querySelector(".btn3").textContent = "";
+    document.querySelector(".btn4").textContent = "";
+    document.querySelector(".btn5").textContent = "";
+    document.querySelector(".btn6").textContent = "";
+    document.querySelector(".btn7").textContent = "";
+    document.querySelector(".btn8").textContent = "";
+    var buttons = document.getElementsByTagName("button");
+    for (var i = 0; i < buttons.length-1; i++) {
+        buttons[i].disabled = false;
+    }
+
+    document.querySelector(".div-win").textContent = "x turn";
+   
+});
